770e7c4c7d7250e3b2ea3a0b1f567a554448355f
weplok <jess.makaronin228@gmail.com>
2024-11-30 21:25:21 +0300

    add trips costs

---
+++ add_cost/1_run_scripts.py
@@ -0,0 +1,12 @@
+import add_trip_cost_2013
+import add_trip_cost_2014
+import add_trip_cost_2015
+import add_trip_cost_2016
+import add_trip_cost_2017
+import add_trip_cost_2018
+import add_trip_cost_2019
+import add_trip_cost_2020
+import add_trip_cost_2021
+import add_trip_cost_2022
+import add_trip_cost_2023
+import add_trip_cost_2024
---
+++ add_cost/add_trip_cost_2020.py
@@ -0,0 +1,65 @@
+import pandas as pd #type: ignore
+import numpy as np #type: ignore
+import math
+
+
+def add_cost(x):
+    if x[2] != "electric_bike":
+        if x[0] == "casual":
+            if x[1] > 45:
+                final_cost = 15
+
+                if x[1] > 180:
+                    final_cost += (round(x[1]) - 180) * 0.15
+
+                return final_cost
+            else:
+                final_cost = 3
+
+                if x[1] > 30:
+                    final_cost += (round(x[1]) - 30) * 0.15
+
+                return final_cost
+
+        else:
+            final_cost = 0
+
+            if x[1] >= 45:
+                final_cost += (round(x[1]) - 45) * 0.15
+
+            return final_cost
+    else:
+        if x[0] == "casual":
+            if x[1] > 45:
+                final_cost = 15
+
+                if x[1] > 180:
+                    final_cost += (round(x[1]) - 180) * 0.20
+
+                return final_cost
+            else:
+                final_cost = 3
+
+                if x[1] > 30:
+                    final_cost += (round(x[1]) - 30) * 0.20
+
+                return final_cost
+
+        else:
+            final_cost = 0
+
+            if x[1] >= 45: 
+                final_cost += (round(x[1]) - 45) * 0.15
+
+            return final_cost
+
+
+df = pd.read_csv("datasets/tripdata-2020.csv")
+df_copy = df.copy()
+df_copy = df_copy.apply(lambda row: [row["member_casual"]] + [row["duration"]] + [row["rideable_type"]], axis=1)
+
+df["cost"] = df_copy.map(add_cost)
+
+print(df.head(15))
+
+df.reset_index().set_index("ride_id").drop('index', axis=1).to_csv("datasets/tripdata-2020.csv")
