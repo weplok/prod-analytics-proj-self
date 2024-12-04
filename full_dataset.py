173b3ceb553c81007ca8775c307188acee2692e6
weplok <jess.makaronin228@gmail.com>
2024-12-04 07:40:44 +0300

    full dataset concat

---
+++ full_dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+main_df = pd.read_csv("mdatasets/tripdata-2013.csv")
+for year in range(2014, 2025):
+    df = pd.read_csv(f"mdatasets/tripdata-{year}.csv")
+    main_df = pd.concat([main_df, df])
+    del df
+main_df = main_df.reset_index().set_index("ride_id").drop('index', axis=1)
+main_df.to_csv("datasets/tripdata-full.csv")
